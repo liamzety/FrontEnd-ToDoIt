7317546c93ff0f8449f9ae9b8dfdaa7d32ca72ef
liamzety <58632095+liamzety@users.noreply.github.com>
2020-11-26 15:36:11 +0200

    favicon and mobile icons

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NotesList } from './NotesList'
 import { AiOutlineFolderAdd } from 'react-icons/ai'
+import logosm from '../assets/img/logo-sm.png'
 
 export function SideBar({ onRemoveNote, notes, onNoteSelect, onAddNote, currNote }) {
     return (
         <div className="sidebar flex align-center col w100">
-            <div className="placeholder">
-
+            <div className="logo-container">
+                <img src={logosm} alt="logo" />
             </div>
             <AiOutlineFolderAdd onClick={onAddNote} />
             <NotesList
