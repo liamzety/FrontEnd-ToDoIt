34a79705bc521ef304bca2a1ad1455335cb64d9e
liamzety <58632095+liamzety@users.noreply.github.com>
2020-12-01 09:41:27 +0200

    user actions async await

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,7 +1,6 @@
 import { userService } from '../../services/userService'
 
 export function loadUser(userId) {
-
     return async dispatch => {
         const user = await userService.query(userId)
         dispatch({ type: 'LOG_USER', user })
@@ -9,54 +8,50 @@ export function loadUser(userId) {
 }
 
 export function login(user) {
-    return dispatch => {
-        return userService.login(user)
-            .then(signedUser => {
-                dispatch({ type: 'LOG_USER', user: signedUser })
-                return user
-            })
-            .catch(({ msg }) => {
-                dispatch({ type: 'SET_MSG', msg: msg || 'Unexpected error, try again later.' })
-                setTimeout(() => {
-                    dispatch({ type: 'RESET_MSG' })
-                }, 3000);
-                throw new Error(msg)
-            })
+    return async dispatch => {
+        try {
+            const signedUser = await userService.login(user)
+            dispatch({ type: 'LOG_USER', user: signedUser })
+            return user
 
+        } catch ({ msg }) {
+            dispatch({ type: 'SET_MSG', msg: msg || 'Unexpected error, try again later.' })
+            setTimeout(() => {
+                dispatch({ type: 'RESET_MSG' })
+            }, 3000);
+            throw new Error(msg)
+        }
     }
 }
 export function signup(user) {
-    return dispatch => {
-        return userService.signup(user)
-            .then(signedUser => {
-                dispatch({ type: 'LOG_USER', user: signedUser })
-                return user
-            })
-            .catch(({ msg }) => {
-                dispatch({ type: 'SET_MSG', msg })
-                setTimeout(() => {
-                    dispatch({ type: 'RESET_MSG' })
-                }, 3000);
-                throw new Error(msg)
-            })
+    return async dispatch => {
+        try {
+            const signedUser = await userService.signup(user)
+            dispatch({ type: 'LOG_USER', user: signedUser })
+            return user
+
+        } catch ({ msg }) {
+            dispatch({ type: 'SET_MSG', msg })
+            setTimeout(() => {
+                dispatch({ type: 'RESET_MSG' })
+            }, 3000);
+            throw new Error(msg)
+        }
     }
 }
 
 export function logout() {
-    return dispatch => {
-        return userService.logout()
-            .then(() => {
-                dispatch({ type: 'LOGOUT' })
-            })
+    return async dispatch => {
+        await userService.logout()
+        dispatch({ type: 'LOGOUT' })
+        return
     }
 }
 
 export function updatePrefs(prefs, user) {
-    return dispatch => {
-        return userService.updatePrefs(prefs, user)
-            .then((updatedUser) => {
-                dispatch({ type: 'UPDATE_USER', updatedUser })
-            })
+    return async dispatch => {
+        const updatedUser = await userService.updatePrefs(prefs, user)
+        dispatch({ type: 'UPDATE_USER', updatedUser })
     }
 }
 
